f237d0901f863c38f47ba6de8a3a830793fa8402
Leonid <leox.07@mail.ru>
2024-11-17 00:27:34 +0300

    Отредактирован 14-й урок

--- lesson_14_a_mans_friend/main.py
+++ lesson_14_a_mans_friend/main.py
@@ -1,27 +1,30 @@
+from pathlib import Path
+import shutil
 import requests
 import os
-# from pathlib import Path
 
-folder = "dogs"
-url = "https://random.dog/woof.json"
-payload = {
-    "filter": "mp4,webm"
-}
-for num in range(10):
-    response = requests.get(url, params=payload)
-    link, picture_extension = os.path.splitext(response.json()["url"])
-    # Второй вариант извлечения расширения
-    # picture_extension = Path(response.json()["url"]).suffix
-    
 
-    filename = f"dog_{num + 1}{picture_extension.lower()}"
-    print(filename)
-    folder_name = "dogs"
-    print(folder_name)
-    if not os.path.isdir(folder_name):
-        os.mkdir(folder_name)
-    file_path = os.path.join(folder_name, filename)
-    print(file_path)
-    print(response.content)
+def get_pic_link(api_url, payload):
+    response = requests.get(api_url, params=payload)
+    return response.json()["url"].lower()
+
+
+def download_picture(folder, filename, url):
+    file_path = os.path.join(folder, filename)
     with open(file_path, "wb") as file:
-        file.write(response.content)
+        file.write(requests.get(url).content)
+
+
+def folder_refresh(folder_path):
+    if Path(folder_path).exists():
+        shutil.rmtree(folder_path)
+    Path(folder).mkdir(parents=True, exist_ok=True)
+
+if __name__ == '__main__':
+    api_url = "https://random.dog/woof.json"
+    folder = "dogs"
+    folder_refresh(folder)
+    for num in range(50):
+        url = get_pic_link(api_url, {"filter": "mp4,webm"})
+        filename = f"dog_{num + 1}{Path(url).suffix}"
+        download_picture(folder, filename, url)
